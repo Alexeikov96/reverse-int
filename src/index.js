1b7af41e01521bfde489346e6ea4d27eb4272d2d
alexeikov96 <speedfaer96@gmail.com>
2023-01-12 23:12:22 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let nModule = String(Math.abs(n));
+    let reverseNumber = '';
+    for (let i = 0; i < nModule.length; i++) {
+       reverseNumber = `${nModule[i]}${reverseNumber}`;
+    }
+    return Number(reverseNumber);
 }
+
+ 
+
