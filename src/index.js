0fd47ab90ce8fbe45ac6139c95344f0b2add132b
alexeikov96 <speedfaer96@gmail.com>
2023-01-13 00:43:10 +0300

    fix: improved index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function reverse (n) {
     let nModule = String(Math.abs(n));
-    let reverseNumber = '';
-    for (let i = 0; i < nModule.length; i++) {
-       reverseNumber = `${nModule[i]}${reverseNumber}`;
-    }
-    return Number(reverseNumber);
+    let reverseNumber = Number(nModule.split('').reverse().join(''));
+    
+    return reverseNumber;
 }
 
  
